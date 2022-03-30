1d1f2478f425ca8c8a6c4ba418431b31aba298cc
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2022-03-30 13:35:31 +0900

    .

---
+++ 백준 1193 분수찾기.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int i = 0;
+	while (1)
+	{		
+		if (n <= 0)
+		{
+			break;
+		}
+		else
+		{
+			n -= i;
+			i++;
+		}
+	}
+	i-=1;
+	if(i%2==0)//짝수줄 라인에 있을때
+	{
+		cout << i + n << "/" <<1 - n <<endl;
+	}
+	else //홀수줄 라인에 있을때
+	{
+		cout<<1 - n<<"/"<< i + n <<endl;
+	}
+	return 0;
+}
