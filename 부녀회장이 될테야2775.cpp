67f6ba52c98cbf7d682a232dea791460a6887452
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2022-03-30 16:46:09 +0900

    Add files via upload
    백준 2775번 부녀회장이 될테야

---
+++ 부녀회장이 될테야2775.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int apt[1000][15];
+int main()
+{
+	int test;
+	cin >> test;
+
+	for (int i = 0; i < test; i++)
+	{
+		int k, n;
+		cin >> k >> n;
+		for (int b = 0; b < 15; b++)
+		{
+			apt[0][b] = b;
+		}
+
+		for (int j = 1; j <= k; j++)//층
+		{
+			for (int q = 1; q <= n; q++)//호수
+			{
+				for (int w = 1; w <= q; w++)
+				{
+					apt[j][q] += apt[j - 1][w];
+				}
+			}
+		}
+		cout << apt[k][n] << endl;
+
+		memset(apt, 0, sizeof(apt));
+	}
+	return 0;
+}
