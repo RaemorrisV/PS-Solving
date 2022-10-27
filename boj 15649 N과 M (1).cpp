31ba1f06dace74a8d69ad4900253d5d04474c96d
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2022-10-27 12:14:17 +0900

    boj 15649 N과 M (1)

---
+++ boj 15649 N과 M (1).cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m;
+vector<int> arr;
+vector<int> ans;
+void dfs(int s)
+{
+    ans.push_back(s);
+    if (ans.size() == m)
+    {
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (find(ans.begin(), ans.end(), arr[i]) == ans.end())
+        {
+            dfs(arr[i]);
+            ans.pop_back();
+        }
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        arr.push_back(i);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        dfs(arr[i]);
+        ans.pop_back();
+    }
+    return 0;
+}
