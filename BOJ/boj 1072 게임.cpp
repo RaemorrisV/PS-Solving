95bf79f7f5926b0b6f62712b64e9aa814b5c1216
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2022-12-19 17:29:07 +0900

    boj 1072 게임

---
+++ BOJ/boj 1072 게임.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long x, y;
+int per;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cin >> x >> y;
+    per = (y * 100 / x);
+    if (per >= 99)
+    {
+        cout << "-1" << '\n';
+    }
+    else
+    {
+        int s = 0;
+        int e = 1000000000;
+        int ans = 0;
+        while (s <= e)
+        {
+            int mid = (s + e) / 2;
+            int p = ((y + mid) * 100 / (x + mid));
+            if (per >= p)
+            {
+                ans = mid + 1;
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
