61c9e891423fce51f194a4e385eef1c5beec7997
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-01-20 15:16:01 +0900

    boj 2302 극장 좌석

---
+++ BOJ/boj 2302 극장 좌석.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int dp[41];
+int n, m;
+bool vip[41];
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> n >> m;
+	for (int i = 1; i <= m; i++)
+	{
+		int input;
+		cin >> input;
+		vip[input] = true;
+	}
+	dp[0] = 1;
+	dp[1] = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		if (vip[i])
+		{
+			dp[i] = dp[i - 1];
+		}
+		else if (vip[i - 1])
+		{
+			dp[i] = dp[i - 1];
+		}
+		else  // 교환이 가능 하다고 생각함
+		{
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+	}
+	/*
+	for (int i = 1; i <= n; i++)
+	{
+		cout << dp[i] << " ";
+	}
+	cout << endl;
+	*/
+	cout << dp[n] << '\n';
+	return 0;
+}
