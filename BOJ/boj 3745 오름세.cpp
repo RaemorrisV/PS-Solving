044b64732329e9457225ce88de482ac13b89396b
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-02-02 16:17:36 +0900

    boj 3745 오름세

---
+++ BOJ/boj 3745 오름세.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<int>ans;
+void init()
+{
+	ans.clear();
+}
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	while (cin >> n)
+	{
+		init();
+		for (int i = 0; i < n; i++)
+		{
+			int a;
+			cin >> a;
+			if (i == 0)ans.push_back(a);
+			else
+			{
+				if (ans.back() < a)
+				{
+					ans.push_back(a);
+				}
+				else
+				{
+					int idx = lower_bound(ans.begin(), ans.end(), a) - ans.begin();
+					ans[idx] = a;
+				}
+			}
+		}
+		cout << ans.size() << "\n";
+	}
+	return 0;
+}
