685b400042aaa6e78eb29e74b1a8ce9efb32e905
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-02-02 15:34:23 +0900

    boj 2776 암기왕

---
+++ BOJ/boj 2776 암기왕.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int t, n, m;
+int arr[1000000];
+void init()
+{
+	memset(arr, 0, sizeof(arr));
+}
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> t;
+	for (int tc = 1; tc <= t; tc++)
+	{
+		init();
+		cin >> n;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+		}
+		sort(arr, arr + n);
+		cin >> m;
+		for (int i = 0; i < m; i++)
+		{
+			int q;
+			cin >> q;
+			int idx = lower_bound(arr, arr + n, q) - arr;
+			if (idx >= n || arr[idx] != q)cout << "0" << '\n';
+			else cout << "1" << "\n";
+		}
+	}
+	return 0;
+}
