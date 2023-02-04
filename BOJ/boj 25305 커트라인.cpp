ef62a81d1f1c04f64115b3137a116f68ed4c0851
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-02-04 16:27:16 +0900

    boj 25305 커트라인

---
+++ BOJ/boj 25305 커트라인.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, k;
+int arr[1000];
+bool compare(int a, int b)
+{
+	return a > b;
+}
+int main()
+{
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + n, compare);
+	cout << arr[k - 1] << '\n';
+	return 0;
+}
