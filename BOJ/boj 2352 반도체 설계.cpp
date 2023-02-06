eea6b5b67a64f70b3b3b73902677e9007d3edb2e
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-02-06 23:14:29 +0900

    boj 2352 반도체 설계

---
+++ BOJ/boj 2352 반도체 설계.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n;
+int arr[40000];
+vector<int>ans;
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+		if (i == 0)ans.push_back(arr[i]);
+		else if (ans.back() < arr[i])ans.push_back(arr[i]);
+		else if (ans.back() >= arr[i])
+		{
+			int idx = lower_bound(ans.begin(), ans.end(), arr[i]) - ans.begin();
+			ans[idx] = arr[i];
+		}
+	}
+	cout << ans.size() << '\n';
+	return 0;
+}
