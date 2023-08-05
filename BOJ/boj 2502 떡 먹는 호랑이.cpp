d5d298019d2bfc72d1c4fa60eaad09d89c1356d4
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-08-05 22:43:06 +0900

    boj 2502 떡 먹는 호랑이

---
+++ BOJ/boj 2502 떡 먹는 호랑이.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int d, k;
+pair<int,int> dp[31];
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> d >> k;
+	dp[1].first = 1;
+	dp[1].second = 0;
+	dp[2].first = 0;
+	dp[2].second = 1;
+	for (int i = 3; i <= d; i++)
+	{
+		dp[i].first = dp[i - 1].first + dp[i - 2].first;
+		dp[i].second = dp[i - 1].second + dp[i - 2].second;
+	}
+	for (int i = 1; i <= k; i++)
+	{
+		if ((k - dp[d].first * i) % dp[d].second == 0)
+		{
+			cout << i << '\n' << (k - dp[d].first * i) / dp[d].second << '\n';
+			break;
+		}
+	}
+	return 0;
+}
