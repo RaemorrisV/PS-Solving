f84cbffc508880adb3e0b13548450eb20a07e26c
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-08-09 14:18:39 +0900

    boj 12000 Circular Barn (Bronze)

---
+++ BOJ/boj 12000 Circular Barn (Bronze).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<int> cow;
+int ans = 1000000000;
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int cowNum;
+		cin >> cowNum;
+		cow.push_back(cowNum);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		int midValue = 0;
+		int moveCount = 1;
+		for (int j = i + 1; j < i + n; j++)
+		{
+			midValue += cow[j % n] * moveCount++;
+		}
+		ans = min(ans, midValue);
+	}
+	cout << ans << "\n";
+	return 0;
+}
