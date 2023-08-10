4ec8de2df9060b762a46ffa35329531553b5d0e1
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-08-10 18:43:59 +0900

    boj 9519 졸려

---
+++ BOJ/boj 9519 졸려.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int x;
+int MOD;
+string word;
+unordered_map<string, bool>duplicated;
+vector<string>arr;
+void simulation()
+{
+	string temp = "";
+	string end = "";
+	for (int i = 0; i < word.length(); i++)
+	{
+		if (i % 2 == 1)
+		{
+			end += word[i];
+		}
+		else temp += word[i];
+	}
+	reverse(end.begin(), end.end());
+	word = temp + end;
+}
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> x >> word;
+	while (duplicated[word]==false)
+	{
+		duplicated[word] = true;
+		arr.push_back(word);
+		simulation();
+		MOD++;
+	}
+	cout << arr[x%MOD] << '\n';
+	return 0;
+}
