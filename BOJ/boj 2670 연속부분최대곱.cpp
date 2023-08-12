ac52f7dac07dc5cc422f5f5ae87f4bfa6e26e310
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-08-12 20:27:29 +0900

    boj 2670 연속부분최대곱

---
+++ BOJ/boj 2670 연속부분최대곱.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+double dp[10001];
+double arr[10001];
+int n;
+double ans;
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
+	dp[1] = arr[1];
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = max(dp[i - 1] * arr[i], arr[i]);
+		ans = max(dp[i], ans);
+	}
+	cout << fixed;
+	cout.precision(3);
+	cout << ans << '\n';
+	return 0;
+}
