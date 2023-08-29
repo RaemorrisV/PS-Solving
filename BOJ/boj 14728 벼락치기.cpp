9161c7b3a49712dc0f252359362ea2b6c28d41d0
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-08-29 15:55:09 +0900

    boj 14728 벼락치기

---
+++ BOJ/boj 14728 벼락치기.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, t, ans;
+vector<pair<int, int>>problem;
+int dp[10001];
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> n >> t;
+	for (int i = 0; i < n; i++)
+	{
+		int k, s;
+		cin >> k >> s;
+		problem.push_back({ k,s });
+	}
+	sort(problem.begin(), problem.end());
+	for (int i = 0; i < problem.size(); i++)
+	{
+		int k = problem[i].first;
+		int s = problem[i].second;
+
+		for (int j = t; j >= k; j--) 
+		{
+			dp[j] = max(dp[j], dp[j - k] + s);
+		}
+	}
+	cout << dp[t] << "\n";
+	return 0;
+}
